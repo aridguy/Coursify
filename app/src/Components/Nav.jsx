27d5e0deb94f62d2afed1ae06fdfcd3b0fef29f8
aridguy <rotimiariyo@gmail.com>
2022-11-03 16:37:08 +0100

    added tailwind and style component setup

---
+++ app/src/Components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <div>
+            <div className="bg-grey-500">
+                dfd
+            </div>
+
+
+
+
+
+
+
+        </div>
+    )
+}
+
+export default Nav
